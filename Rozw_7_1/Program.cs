8e3f9744219d10e92f8a17a9d288a2ba173461e1
Piotrek <@>
2017-11-16 17:34:34 +0100

    init

---
+++ Rozw_7_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    sealed class Osoba
+    {
+
+    }
+
+    class Student:Osoba
+    {
+
+    }
+}
